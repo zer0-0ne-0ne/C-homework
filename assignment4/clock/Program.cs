00d81b8a59a32fc907e29b446c5ad7bf92554aae
zer0-0ne-0ne <3389511665@qq.com>
2025-03-19 22:29:07 +0800

    Add files via upload

---
+++ assignment4/clock/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _3_14_1
+{
+    public delegate void TickEventHandler(object sender, EventArgs e);
+
+    // 定义响铃事件委托
+    public delegate void AlarmEventHandler(object sender, EventArgs e);
+
+    // 闹钟类
+    public class AlarmClock
+    {
+        private int alarmTime;
+        private int currentTime = 0;
+
+        public event TickEventHandler Tick;
+        public event AlarmEventHandler Alarm;
+        public AlarmClock(int alarmTime)
+        {
+            this.alarmTime = alarmTime;
+        }
+        public void Start()
+        {
+            while (true)
+            {
+                OnTick();
+                currentTime++;
+
+                if (currentTime == alarmTime)
+                {
+                    OnAlarm();
+                    break;
+                }
+            }
+        }
+        protected virtual void OnTick()
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+        }
+        protected virtual void OnAlarm()
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("请输入闹钟的响铃时间:");
+            string s = "";
+            s = Console.ReadLine();
+            n = int.Parse(s);
+            AlarmClock clock = new AlarmClock(n);
+            clock.Tick += (sender, e) =>
+            {
+                Console.WriteLine("滴答");
+            };
+            clock.Alarm += (sender, e) =>
+            {
+                Console.WriteLine("响铃");
+            };
+            clock.Start();
+        }
+    }
+}
